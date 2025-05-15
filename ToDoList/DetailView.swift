ff0ea5c5f2ed8e8fa586b4686d0b4f80ae7c05e1
Dan Mit <danmit25@icloud.com>
2025-05-15 21:06:54 +0200

    det

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -8,27 +8,56 @@
 import SwiftUI
 
 struct DetailView: View {
-    var passedValue: String // Don't initialize it - it will be passed from the parent view
+    @State var toDo: String
+    @State private var reminderIsOn = false
+//    @State private var dueDate = Date.now + 60*60*24
+    @State private var dueDate = Calendar.current.date(byAdding: .day, value: 1 , to: .now)!
+    @State private var notes = ""
+    @State private var isCompleted = false
+    
     @Environment(\.dismiss) private var dismiss
     var body: some View {
-        VStack {
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            Text("You Are A Swifty Legend!\nAnd you passsed over the value \(passedValue)")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            Spacer()
-            Button("Get Back!") {
-                dismiss()
-            }
-            .buttonStyle(.borderedProminent)
+        List {
+            TextField("Enter To Do here", text: $toDo)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
+            Toggle("Set Reminder", isOn: $reminderIsOn)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            DatePicker("Date", selection: $dueDate)
+                .listRowSeparator(.hidden)
+                .padding(.bottom)
+                .disabled(!reminderIsOn)
+            Text("Notes")
+                .padding(.top)
+            TextField("Notes", text: $notes, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .listRowSeparator(.hidden)
+            Toggle("Completed", isOn: $isCompleted)
+                .padding(.top)
+                .listRowSeparator(.hidden)
         }
-        .padding()
+        .listStyle(.plain)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    //TODO: Add Save Code Here
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    DetailView(passedValue: "Item 1")
+    NavigationStack {
+        DetailView(toDo: "")
+    }
 }
--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -13,20 +13,37 @@ struct ToDoListView: View {
                  "Change the World",
                  "Bring the Awesome",
                  "Take a Vacation"]
+    @State private var sheetIsPresented = false
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(toDos, id: \.self) { toDo in
                     NavigationLink {
-                        DetailView(passedValue: toDo)
+                        DetailView(toDo: toDo)
                     } label: {
                         Text(toDo)
                     }
+                    .font(.title2)
                 }
             }
             .navigationTitle("To Do List")
             .navigationBarTitleDisplayMode(.automatic)
             .listStyle(.plain)
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack {
+                    DetailView(toDo: "")
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
         }
     }
 }
